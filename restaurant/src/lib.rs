08bf227e8434983c9413d92bca203dd83cda0536
Jan Vorisek <vorisek.jan@outlook.com>
2022-01-16 17:53:33 +0100

    add ch 7 example

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,76 @@
+
+// use std::{cmp::Ordering, io}; // shared subpath
+// use std::io::{self, Write};   // merge paths
+// use std::collections::*;      // glob
+mod front_of_house;
+pub use crate::front_of_house::hosting;
+
+fn serve_order() {}
+
+mod back_of_house {
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::serve_order();
+    }
+
+    fn cook_order() {}
+}
+
+
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+
+    //use crate::front_of_house::hosting;
+    use self::front_of_house::hosting;
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+
+    use self::front_of_house::hosting::add_to_waitlist;
+    add_to_waitlist();
+    add_to_waitlist();
+    add_to_waitlist();
+
+    use self::front_of_house::hosting::add_to_waitlist as add;
+    add();
+    add();
+    add();
+}
